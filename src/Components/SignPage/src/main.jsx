01624d704e6b6919bca5afb1a141fb5c081971a4
LoubnaIdm <loubnaidm1@gmail.com>
2024-03-30 17:26:52 +0100

    add SignPage

---
+++ src/Components/SignPage/src/main.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+)
