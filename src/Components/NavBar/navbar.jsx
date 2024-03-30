afeb7c39ea8c8439ed796ed5d2504db0cd16b9f9
Raihanala <raihana.lahsinia@gmail.com>
2024-03-30 16:04:47 +0000

    navbarV1

--- src/Components/NavBar/navbar.jsx
+++ src/Components/NavBar/navbar.jsx
@@ -3,6 +3,23 @@ import ';/navbar.css'
 const navbar = () => {
   return (
     <div className='navbar'>
+        <img src={logo} alt="" className="logo"/>
+        <ul className="navbar-menu">
+            <li>Home</li>
+            <li>Menu</li>
+            <li>---</li>
+            <li>Contact-us</li>
+        </ul>
+        <div className="navbar-right">
+            <img src={search} alt=""/>
+            <div className="navbar-search-icon">
+                <img src={backet-icon} alt="" />
+                <div className="dot">
+                    <button>Sign In</button>
+                    </div>              
+            </div>
+        </div>
+        
       
     </div>
   )
