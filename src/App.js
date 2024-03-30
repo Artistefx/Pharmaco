827d9a2c2ee4087b9755eac12be54db7ef9d6483
Raihanala <raihana.lahsinia@gmail.com>
2024-03-30 15:12:06 +0000

    Rey's try
    C
    rey try
    :wq

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bienvenue sur notre application !</h1>
+      <p>Ceci est la page d'accueil de notre application.</p>
+      {/* Ajoutez d'autres composants ou contenu ici */}
     </div>
   );
-}
+};
 
 export default App;
