397fed0428f7d33bf69fed27191bdff3aa558a6a
Artistefx <114290288+Artistefx@users.noreply.github.com>
2024-03-30 15:23:53 +0000

    add Welcome

---
+++ src/Components/Welcome.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import pharmaco from "../Images/pharmaco.webp";
+
+const Welcome = () => {
+  return (
+    <div className="WelcomeImg">
+      <div className="ImageContainer">
+        <img src={pharmaco} alt="pharmaco" />
+      </div>
+      <div className="ButtonContainer">
+        <a href="http://localhost:3000/">Antibiotiques</a>
+        <a href="http://localhost:3000/">Anti-inflammatoires</a>
+        <a href="http://localhost:3000/">suppléments</a>
+        <a href="http://localhost:3000/">Equipement</a>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
