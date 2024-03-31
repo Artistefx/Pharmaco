edc70d0203721a88b706af0a5002299048f02e81
meryemMekawi <mekawimeryem@gmail.com>
2024-03-31 23:55:37 +0000

    login Page edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Welcome from './Components/Welcome/Welcome';
+//import Welcome from './Components/Welcome/Welcome';
+import PageLogin  from './Components/loginPage/PageLogin.jsx'
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <PageLogin />
     </div>
   );
 }
