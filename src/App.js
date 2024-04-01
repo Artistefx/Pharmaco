85bb76427990989428a87b71433b1213f618a88a
LoubnaIdm <loubnaidm1@gmail.com>
2024-04-01 22:57:24 +0200

    Add signup page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Welcome from './Components/Welcome/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
