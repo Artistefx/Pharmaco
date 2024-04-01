fdd44837a8d28e055a4dc8570af1f4756ebe5e57
LoubnaIdm <loubnaidm1@gmail.com>
2024-04-01 20:45:07 +0200

    add signUp page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 //import Welcome from './Components/Welcome/Welcome';
-import PageLogin  from './Components/loginPage/PageLogin.jsx'
-
+//import PageLogin  from './Components/loginPage/PageLogin.jsx'
+import Sign from './Components/SignupPage/sign.jsx'
 function App() {
   return (
     <div className="App">
-      <PageLogin />
+    
+      <Sign />
     </div>
   );
 }
