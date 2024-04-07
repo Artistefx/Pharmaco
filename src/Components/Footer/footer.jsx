0e1495489de7e3bb5de1e78bb2a5bade75138167
LoubnaIdm <loubnaidm1@gmail.com>
2024-04-07 23:37:35 +0200

    add footer

---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,51 @@
+import './footerStyle.css';
+import React from 'react';
+import logo from './images/logo.png';
+import { FaFacebookF, FaInstagram, FaWhatsapp, FaTwitter } from 'react-icons/fa';
+
+function Footer() {
+  return (
+<body>
+    <footer>
+        <div className='row'>
+        <div className='col'>
+       <div className='log'> <img src={logo} alt="Pharmaco Logo" /> </div>
+      <p className='p1'>Nous offrons une vaste gamme de médicaments de qualité, livrés directement à votre porte.  </p>
+        </div>
+        <div className='col'>
+            <h3>Contacts <div className='underline'><span></span></div></h3>
+            <hr className='hr1'></hr>
+            <p>Hay riad rue 18 Khouribga</p>
+            <p className='email-id'>Matsswlnich@gmail.com</p>
+            <h4>+212 - 649184760</h4>
+        </div>
+        <div className='col'>
+        <h3>Links <div className='underline'><span></span></div></h3>
+        <hr className='hr1'></hr>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">Features</a></li>
+                
+            </ul>
+        </div>
+        <div className='col'>
+            <h3>Find us on : <div className='underline'><span></span></div></h3>
+            <hr className='hr1'></hr>
+            <div className='social-icons'>
+            <i className='fab'><FaFacebookF /></i>
+            <i className='fab'><FaInstagram /></i>
+            <i className='fab'><FaWhatsapp /></i>
+            <i className='fab'><FaTwitter /></i>
+                </div>
+         </div>
+         </div>
+         <hr></hr>
+         <p className='copyright'>&copy; 2024 Pharmaco - All rights reserved.</p>
+    </footer>
+ </body>
+  );
+}
+
+export default Footer
