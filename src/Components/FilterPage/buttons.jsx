bd7586b92990077781743ba20cbe565921a377ef
LoubnaIdm <loubnaidm1@gmail.com>
2024-05-02 00:39:49 +0200

    premiere version

---
+++ src/Components/FilterPage/buttons.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Data from './data';
+
+function Buttons({ menuItems, filterItems, setItems }) {
+    return (
+        <div className='d-flex justify-content-center mb-2'>
+            <button className='btn btn-success btn-custom-size text-white p-2 px-4 mx-2 btn-lg fw-bold'
+                onClick={() => setItems(Data)}>
+                Tous
+            </button>
+            {menuItems.map(val => (
+                <button className='btn btn-success btn-custom-size text-white p-2 px-4 mx-2 btn-lg fw-bold'
+                    onClick={() => filterItems(val)}>
+                    {val}
+                </button>
+            ))}
+            <style jsx>{`
+                .btn-success {
+                    background-color: #2da48c;
+                    border-color: #2da48c;
+                }
+
+                .btn-success:hover {
+                    background-color: #84cebe;
+                    border-color: #84cebe;
+                }
+
+                .btn-success:focus,
+                .btn-success.focus {
+                    box-shadow: 0 0 0 0.2rem rgba(76, 175, 80, 0.5);
+                }
+
+                .btn-success:active,
+                .btn-success.active,
+                .show > .btn-success.dropdown-toggle {
+                    background-color: #2da48c;
+                    border-color: #2da48c;
+                }
+
+                .btn-success.disabled,
+                .btn-success:disabled {
+                    background-color: #2da48c;
+                    border-color: #2da48c;
+                }
+
+                /* Ajoutez une classe pour définir la taille des boutons */
+                .btn-custom-size {
+                    font-size: 16px; /* Définissez la taille de la police souhaitée */
+                    padding: 5px 10px; /* Définissez le rembourrage souhaité */
+                }
+            `}</style>
+        </div>
+    );
+}
+
+export default Buttons;
