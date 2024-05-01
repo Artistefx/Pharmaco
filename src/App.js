da5d25eec7c870579239becccc9c7d18f8905f11
Raihanala <raihana.lahsinia@gmail.com>
2024-05-01 16:30:25 +0100

    commit1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import Welcome from './Components/Welcome/Welcome';
-
 import ProductDisplay from './Components/ProductDisplay/ProductDisplay';
 import ProductPage from './Components/ProductDisplay/ProductPage';
 import ShuffleHero from './Components/Welcome/ShuffleHero';
 import SignUp from './Components/SignupPage/sign';
 import Login from './Components/loginPage/PageLogin';
-
+import Navbar from './Components/NavBar/navbar';
 
 function App() {
   return (
     <div className="App">
-      <Welcome/>
+      <Navbar />
     </div>
   );
 }
-
 export default App;
