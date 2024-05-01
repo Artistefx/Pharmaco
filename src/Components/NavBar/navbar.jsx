2f27f3004d71a13dc5a14a2779d7d15ab0e84a25
Raihanala <raihana.lahsinia@gmail.com>
2024-05-01 17:49:00 +0100

    Ajout des fichiers non suivis

--- src/Components/NavBar/navbar.jsx
+++ src/Components/NavBar/navbar.jsx
@@ -2,12 +2,20 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import React from 'react';
-import userIcon from '../../Assets/icons8-utilisateur-sexe-neutre-64.png';
+import userIcon from './Assets/icons8-utilisateur-sexe-neutre-64.png';
+import searchIcon from './Assets/icons8-chercher-50.png'
+import panierIcon from './Assets/icons8-panier-64.png'
+const products = [
+  { name: 'Product 1', href: '#' },
+  { name: 'Product 2', href: '#' },
+  { name: 'Product 3', href: '#' },
+  { name: 'Product 4', href: '#' },
+];
 const navigation = [
-  { name: 'Home', href: './Components/Welcome/Welcome', current: true },
+  { name: 'Home', href: './Components/Welcome/Welcome', current: false },
   { name: 'Product', href: './Components/ProductDisplay/ProductPage', current: false },
-  { name: 'Projects', href: './Components/SignupPage/sign', current: false },
-  { name: 'About', href: './Components/SignupPage/sign', current: false },
+  { name: 'New arrivals', href: './Components/SignupPage/sign', current: false },
+  { name: 'Our company', href: './Components/SignupPage/sign', current: false },
 ]
 
 function classNames(...classes) {
@@ -16,63 +24,71 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-green-200">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
+              {/* Logo */}
+              <div>
+                <img
+                  className="h-8 w-auto"
+                  src="/logo192.png"
+                  alt="Your Company"
+                />
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="/logo192.png"
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
+              {/* Navigation links */}
+              <div className="hidden sm:flex sm:flex-1 sm:justify-center">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-green-200 text-black' : 'text-black hover:bg-green-200 hover:text-white',
+                        'rounded-md px-3 py-2 text-sm font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
+              {/* Icons and profile dropdown */}
+              <div className="flex items-center space-x-4">
+                <a href="./Components/Welcome/Welcome">
+                  <button
+                    type="button"
+                    className="relative rounded-full bg-green-200 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-200"
+                  >
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">View search</span>
+                    <img
+                      className="h-6 w-6 rounded-full"
+                      src={panierIcon}
+                      alt=""
+                    />
+                  </button>
+                </a>
+                <a href="./Components/Welcome/Welcome">
+                  <button
+                    type="button"
+                    className="relative rounded-full bg-green-200 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-200"
+                  >
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">View search</span>
+                    <img
+                      className="h-6 w-6 rounded-full"
+                      src={searchIcon}
+                      alt=""
+                    />
+                  </button>
+                </a>
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className="relative flex rounded-full bg-green-200 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-200">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
@@ -98,7 +114,7 @@ export default function Example() {
                             href="./Components/SignupPage/sign"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
-                            Your Profile
+                            Sign In
                           </a>
                         )}
                       </Menu.Item>
@@ -108,7 +124,7 @@ export default function Example() {
                             href="./Components/SignupPage/sign"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
-                            Settings
+                            Sign Up 
                           </a>
                         )}
                       </Menu.Item>
@@ -127,26 +143,26 @@ export default function Example() {
                 </Menu>
               </div>
             </div>
+            {/* Mobile menu */}
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 px-2 pb-3 pt-2">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
